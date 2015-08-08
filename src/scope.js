62a4f0852b83ad8e08285ebfc4872a0154af0813
Jason Ody <jasonody@users.noreply.github.com>
2015-08-07 20:05:05 -0700

    Scope: $$postDigest for executing code after a digest

--- src/scope.js
+++ src/scope.js
@@ -9,6 +9,7 @@ function Scope() {
 	this.$$asyncQueue = [];
 	this.$$applyAsyncQueue = [];
 	this.$$applyAsyncId = null;
+	this.$$postDigestQueue = [];
 	this.$$phase = null;
 }
 
@@ -79,6 +80,10 @@ Scope.prototype.$digest = function () {
 	} while (dirty || this.$$asyncQueue.length);
 	
 	this.$clearPhase();
+	
+	while (this.$$postDigestQueue.length) {
+		this.$$postDigestQueue.shift()();
+	}
 };
 
 Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
@@ -162,4 +167,9 @@ Scope.prototype.$$flushApplyAsync = function () {
 	}
 
 	this.$$applyAsyncId = null;
+};
+
+Scope.prototype.$$postDigest = function (fn) {
+	
+	this.$$postDigestQueue.push(fn);
 };
