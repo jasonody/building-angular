b0c40090f6460a8dc59136f394340c131f747aec
Jason Ody <jasonody@users.noreply.github.com>
2015-09-25 14:33:42 -0700

    Scope: $watchCollection - detecting new or replaced attributes in objects and handling NaNs

--- src/scope.js
+++ src/scope.js
@@ -362,6 +362,15 @@ Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
 					changeCount++;
 					oldValue = {};
 				}
+				
+				_.forOwn(newValue, function (newVal, key) {
+					
+					var bothNaN = _.isNaN(newVal) && _.isNaN(oldValue[key]);
+					if (!bothNaN && oldValue[key] !== newVal) {
+						changeCount++;
+						oldValue[key] = newVal;
+					}
+				});
 			}
 		} else { //everything else that isn't an object
 			if (!self.$$areEqual(newValue, oldValue, false)) { //3rd arg indicates to use reference comparison
