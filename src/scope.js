a389a47045a06415b3dfb8db10e86091d850442d
Jason Ody <jasonody@users.noreply.github.com>
2015-10-29 22:00:52 -0700

    Scope: $on - registering event listeners

--- src/scope.js
+++ src/scope.js
@@ -12,6 +12,7 @@ function Scope() {
 	this.$$postDigestQueue = [];
 	this.$root = this;
 	this.$$children = [];
+	this.$$listeners = {};
 	this.$$phase = null;
 }
 
@@ -296,6 +297,7 @@ Scope.prototype.$new = function (isolated, parent) {
 	
 	parent.$$children.push(child);
 	child.$$watchers = [];
+	child.$$listeners = {};
 	child.$$children = [];
 	child.$parent = parent;
 	
@@ -424,4 +426,13 @@ Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
 	};
 	
 	return this.$watch(internalWatchFn, internalListenerFn);
+};
+
+Scope.prototype.$on = function (eventName, listener) {
+	
+	var listeners = this.$$listeners[eventName];
+	if (!listeners) {
+		this.$$listeners[eventName] = listeners = [];
+	}
+	listeners.push(listener);
 };
